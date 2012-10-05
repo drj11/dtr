4d19c2d68beb3164decbaecc4fd30855f90706b0
David Jones <drj@pobox.com>
2012-10-05 15:10:59 +0000

    sync_dmet

--- sequence.sh
+++ sequence.sh
@@ -2,6 +2,17 @@
 # sequence
 # do everything in sequence.
 
+IFS=''
+
+sync_dmet () {
+    # The work/dmet directory is derived from the data/ghcnd_gsn directory.
+    if [ $(ls work/dmet|wc -l) -eq $(ls data/ghcnd_gsn|wc -l) ]
+    then
+        return
+    fi
+    ./massdmet.sh
+}
+
 mkdir -p data
 if ! test -e data/ghcnd_gsn.tar.gz
 then
@@ -17,3 +28,4 @@ then
     zcat ghcnd_gsn.tar.gz | tar xvf -
     )
 fi
+sync_dmet
