7c30416fdb75139014e1889aa38f017a074d865c
David Jones <drj@pobox.com>
2012-10-14 21:30:30 +0100

    funky list thingy

--- ghcn.R
+++ ghcn.R
@@ -17,7 +17,7 @@ ghcnm.station <- function(station, dir='work/mdtr') {
   return(s)
 }
 ghcnm.station.element <- function(station, element, dir='work/mdtr') {
-  s <- ghcnm.station(station)
+  s <- ghcnm.station(station, dir=dir)
   s <- s[s[, 3]==element, ]
   return(s)
 }
@@ -33,7 +33,35 @@ Months <- function(s) {
   res[m] <- do.call(cbind, s[, seq(4, 27, 2)])
   # Remove data with the MISSING value.
   res[res==-9999] <- NA
+  # Only appears in DMET, but still worth removing.
+  res[res==8888] <- NA
   return(res)
+}
+ghcnm.station.list <- function(station, element, dir='work/mdtr') {
+  s <- ghcnm.station.element(station, element, dir=dir)
+  m <- Months(s)
+  if (is.element(element, c('TMIN', 'TMAX', 'MDTR', 'DMET'))) {
+    m <- m * 0.01
+  }
+  r <- range(s[, 2])
+  res <- list(uid=station, df=s, baseyear=r[1], lastyear=r[2], element=element, series=m)
+  return(res)
+}
+PlotM <- function(sl) {
+  # Station as list (eg result from ghcnm.station.list)
+  unit <- 'mystery units'
+  if (is.element(sl$element, c('TMIN', 'TMAX'))) {
+    unit <- 'C'
+  }
+  if (is.element(sl$element, c('MDTR', 'DMET'))) {
+    unit <- 'K'
+  }
+  ylab <- paste(sl$element, unit, sep=' ')
+  x <- sl$baseyear+((1:length(sl$series))-0.5)/12
+  y <- sl$series
+  plot(x, y,
+    main=paste('GHCN-D ', sl$uid, sep=''), xlab='year', ylab=ylab)
+  return(list(x=x, y=y))
 }
 
 ExpandDays <- function(x) {
